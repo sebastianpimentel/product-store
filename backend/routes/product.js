85e5187e442ff98895f651877184435fa8f52f19
sebastian pimentel <sebaspimen011@gmail.com>
2021-08-10 11:29:02 -0500

    first version_1 of the pepita warehouse project

--- backend/routes/product.js
+++ backend/routes/product.js
@@ -3,10 +3,10 @@ const router= express.Router();
 const productController= require("../controllers/product")
 
 //get post put delete
-//http://localhost:3001/api/role/registerRole asi va a quedar la ruta de api que estamos creando
+//http://localhost:3001/api/product/registerProduct asi va a quedar la ruta de api que estamos creando
 router.post("/registerProduct", productController.registerProduct);//para registrar
 
-//http://localhost:3001/api/role/listRole
+//http://localhost:3001/api/product/listProduct
 router.get("/listProduct",productController.listProduct);//para listar
 
 module.exports=router;
